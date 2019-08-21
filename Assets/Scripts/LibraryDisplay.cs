bb6ce3d24be57d26df7e83832af03125772043b0
ahub <ahub@riseup.net>
2019-08-21 08:34:28 +0200

    Add demo library scene

---
+++ Assets/Scripts/LibraryDisplay.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class LibraryDisplay : MonoBehaviour
+{
+    [SerializeField] private Card cardPrefab;
+
+    [SerializeField] private Transform content;
+    //TODO: CardPack separator
+}
